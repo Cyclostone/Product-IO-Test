c76423687ae62a29ec7f1fcdc5a795a2f61e01bc
Arpit Shrotriya <arpit.shrotriya5945@gmail.com>
2021-08-02 12:30:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,401 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "Nunito",
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int _pageState = 0;
+
+  var _backgroundColor = Colors.white;
+  var _headingColor = Color(0xffb40284a);
+
+  double _headingTop = 100;
+
+  double _loginWidth = 0;
+  double _loginHeight = 0;
+  double _loginOpacity = 1;
+
+  double _loginYOffset = 0;
+  double _loginXOffset = 0;
+  double _registerYOffset = 0;
+  double _registerHeight = 0;
+
+  double windowWidth = 0;
+  double windowHeight = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+
+    _loginHeight = windowHeight - 270;
+    _registerHeight = windowHeight - 270;
+
+    switch (_pageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        _headingColor = const Color(0xffb40284a);
+
+        _headingTop = 100;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = windowHeight;
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
+        break;
+      case 1:
+        _backgroundColor = Color(0xffbd34c59);
+        _headingColor = Colors.white;
+
+        _headingTop = 90;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = 270;
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
+        break;
+      case 2:
+        _backgroundColor = Color(0xffbd34c59);
+        _headingColor = Colors.white;
+
+        _headingTop = 80;
+
+        _loginWidth = windowWidth - 40;
+        _loginOpacity = 0.7;
+
+        _loginYOffset = 240;
+        _loginXOffset = 20;
+        _registerYOffset = 270;
+        break;
+    }
+
+    return Material(
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: const Duration(
+              milliseconds: 2000,
+            ),
+            color: _backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _pageState = 0;
+                    });
+                  },
+                  child: Container(
+                    child: Column(
+                      children: [
+                        AnimatedContainer(
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          duration: const Duration(
+                            milliseconds: 1000,
+                          ),
+                          margin: EdgeInsets.only(top: _headingTop),
+                          child: Text(
+                            'Product IO Test',
+                            style: TextStyle(
+                              color: _headingColor,
+                              fontSize: 28,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(20),
+                          padding: const EdgeInsets.symmetric(horizontal: 32),
+                          child: Text(
+                            'This is my submission for the Internship Assignment',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: _headingColor,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Center(
+                    child: Center(
+                      child: Image.asset('assets/images/splash_bg.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Clicked');
+                      setState(() {
+                        if (_pageState != 0) {
+                          _pageState = 0;
+                        } else {
+                          _pageState = 1;
+                        }
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(32),
+                      padding: const EdgeInsets.all(20),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color(0xffb40284a),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          AnimatedContainer(
+            padding: const EdgeInsets.all(20),
+            width: _loginWidth,
+            height: _loginHeight,
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: const Duration(
+              milliseconds: 1000,
+            ),
+            transform:
+                Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(_loginOpacity),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: const Text(
+                        'Login To Continue',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    const InputWithIcon(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const InputWithIcon(),
+                  ],
+                ),
+                Column(
+                  children: [
+                    PrimaryButton(btnText: 'Login'),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _pageState = 2;
+                        });
+                      },
+                      child: _OutlineBtn(btnText: 'Create New Account'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          AnimatedContainer(
+            height: _registerHeight,
+            padding: const EdgeInsets.all(32),
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: const Duration(
+              milliseconds: 1000,
+            ),
+            transform: Matrix4.translationValues(0, _registerYOffset, 1),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: const Text(
+                        'Create a New Account',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    PrimaryButton(btnText: 'Create Account'),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _pageState = 1;
+                        });
+                      },
+                      child: _OutlineBtn(btnText: 'Back To Login'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InputWithIcon extends StatefulWidget {
+  const InputWithIcon({Key? key}) : super(key: key);
+
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0xffb40284a),
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 60,
+            child: Icon(
+              Icons.email,
+              size: 20,
+              color: Color(0xffbb9b9b9),
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 20),
+                hintText: 'Enter Email...',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  final String btnText;
+  PrimaryButton({required this.btnText});
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffb40284a),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _OutlineBtn extends StatefulWidget {
+  final String btnText;
+  _OutlineBtn({required this.btnText});
+
+  @override
+  __OutlineBtnState createState() => __OutlineBtnState();
+}
+
+class __OutlineBtnState extends State<_OutlineBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0xffb40284a),
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: const TextStyle(
+            color: Color(0xffb40284a),
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
